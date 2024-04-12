4bd683d89ecb855499c800f288c81b70ee41ffce
Shiwang0-0 <bisht0shiwang@gmail.com>
2024-04-12 06:02:31 +0530

    added protected routes while navigating

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,20 +1,32 @@
 import React,{lazy} from 'react'
 import {BrowserRouter,Routes,Route} from 'react-router-dom'
+import ProtectedRoute from './components/styles/auth/ProtectedRoute.jsx';
 
 const Home= lazy(()=>import("./pages/Home.jsx"));
 const Login=lazy(()=>import("./pages/Login.jsx"))
 const Chat=lazy(()=>import("./pages/Chat.jsx"))
 const Group=lazy(()=>import("./pages/Group.jsx"))
+const NotFound=lazy(()=>import("./pages/NotFound.jsx"))
 
 const App = () => {
+  let user=true;
   return (
     <BrowserRouter>
-   
-    <Routes>
-      <Route path="/" element={<Home/>}/>
-      <Route path="/chat/:chatId" element={<Chat/>}/>
-      <Route path="/group" element={<Group/>}/>
-      <Route path="/login" element={<Login/>}/>
+    
+    <Routes>  
+      <Route element={<ProtectedRoute user={user}/>}>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/chat/:chatId" element={<Chat/>}/>
+        <Route path="/group" element={<Group/>}/>
+      </Route>
+
+      <Route path="/login" element={<ProtectedRoute user={!user} redirect="/">
+        <Login/>
+      </ProtectedRoute>
+      }/>
+
+      <Route path="*" element={<NotFound/>}/>
+
     </Routes>
     </BrowserRouter>
   )
