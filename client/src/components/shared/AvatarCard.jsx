e88dd89d8f941b35ad2124422ca786dbbcabd7bf
Shiwang0-0 <bisht0shiwang@gmail.com>
2024-04-18 05:02:59 +0530

    Avatar in Chat Item

---
+++ client/src/components/shared/AvatarCard.jsx
@@ -0,0 +1,23 @@
+import { Avatar, AvatarGroup, Stack,Box } from '@mui/material'
+import React from 'react'
+
+const AvatarCard = ({avatar=[], max=4}) => {
+  return (
+    <Stack direction={"row"} spacing={0.5}>
+        <AvatarGroup max={max}>
+            <Box sx={{width:"5rem", height:"2rem",  position: "relative"}} > 
+            {avatar.map((src,index)=>(
+                <Avatar 
+                src={src} key={Math.random()*100} alt={`Avatar ${index}`} sx={{height:"2.2rem" ,width:"2.2rem",position: "absolute", 
+                left:`${0.4 + index}rem`,
+                right:`${0.4 + index}rem`,
+                }}
+                />
+            ))}
+            </Box>
+        </AvatarGroup>
+    </Stack>
+  )
+}
+
+export default AvatarCard
