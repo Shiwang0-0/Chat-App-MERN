96e570e837cc6109c43d21ebab81c47659178a4e
Shiwang0-0 <bisht0shiwang@gmail.com>
2024-04-18 05:03:42 +0530

    sample data for chats

---
+++ client/src/constants/sampleChats.js
@@ -0,0 +1,16 @@
+export const sampleChats=[
+    {
+    name:"hulala",
+    avatar:["https://cdn.pixabay.com/photo/2016/05/05/02/37/sunset-1373171_1280.jpg"],
+    _id:"1",
+    groupChat:"false",
+    members:["1","2"]
+    },
+    {
+      name:"sdfsdfadf",
+      avatar:["https://cdn.pixabay.com/photo/2016/05/05/02/37/sunset-1373171_1280.jpg","https://cdn.pixabay.com/photo/2016/05/05/02/37/sunset-1373171_1280.jpg","https://cdn.pixabay.com/photo/2016/05/05/02/37/sunset-1373171_1280.jpg","https://cdn.pixabay.com/photo/2016/05/05/02/37/sunset-1373171_1280.jpg"],
+      _id:"2",
+      groupChat:"true",
+      members:["1","2"]
+      },
+  ] 
