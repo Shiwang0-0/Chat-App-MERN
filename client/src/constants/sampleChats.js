2f6619b3c1e5b86ca3a974a3d1393f79a1736c0c
Shiwang0-0 <bisht0shiwang@gmail.com>
2024-04-29 03:19:56 +0530

    sample chats added

--- client/src/constants/sampleChats.js
+++ client/src/constants/sampleChats.js
@@ -1,47 +1,116 @@
-export const sampleChats=[
-    {
-    name:"hulala",
-    avatar:["https://cdn.pixabay.com/photo/2016/05/05/02/37/sunset-1373171_1280.jpg"],
-    _id:"1",
-    groupChat:"false",
-    members:["1","2"]
-    },
-    {
-      name:"sdfsdfadf",
-      avatar:["https://cdn.pixabay.com/photo/2016/05/05/02/37/sunset-1373171_1280.jpg","https://cdn.pixabay.com/photo/2016/05/05/02/37/sunset-1373171_1280.jpg","https://cdn.pixabay.com/photo/2016/05/05/02/37/sunset-1373171_1280.jpg","https://cdn.pixabay.com/photo/2016/05/05/02/37/sunset-1373171_1280.jpg"],
-      _id:"2",
-      groupChat:"true",
-      members:["1","2"]
-      },
-  ] 
+export const sampleChats = [
+  {
+    name: "hulala",
+    avatar: ["https://cdn.pixabay.com/photo/2016/05/05/02/37/sunset-1373171_1280.jpg"],
+    _id: "1",
+    groupChat: "false",
+    members: ["1", "2"]
+  },
+  {
+    name: "sdfsdfadf",
+    avatar: ["https://cdn.pixabay.com/photo/2016/05/05/02/37/sunset-1373171_1280.jpg", "https://cdn.pixabay.com/photo/2016/05/05/02/37/sunset-1373171_1280.jpg", "https://cdn.pixabay.com/photo/2016/05/05/02/37/sunset-1373171_1280.jpg", "https://cdn.pixabay.com/photo/2016/05/05/02/37/sunset-1373171_1280.jpg"],
+    _id: "2",
+    groupChat: "true",
+    members: ["1", "2"]
+  },
+]
 
 
-export const sampleSearchs=[
-    {
-    name:"hulala",
-    _id:"1",
-    avatar:"https://cdn.pixabay.com/photo/2016/05/05/02/37/sunset-1373171_1280.jpg"
-    },
-    {
-      name:"sdfsdfadf",
-      _id:"2",
-      avatar:"https://cdn.pixabay.com/photo/2016/05/05/02/37/sunset-1373171_1280.jpg"
-      },
-  ] 
+export const sampleSearchs = [
+  {
+    name: "hulala",
+    _id: "1",
+    avatar: "https://cdn.pixabay.com/photo/2016/05/05/02/37/sunset-1373171_1280.jpg"
+  },
+  {
+    name: "sdfsdfadf",
+    _id: "2",
+    avatar: "https://cdn.pixabay.com/photo/2016/05/05/02/37/sunset-1373171_1280.jpg"
+  },
+]
 
-  export const sampleNotifications=[
-    {
-      sender:{
-        name:"hulala",
-        avatar:"https://cdn.pixabay.com/photo/2016/05/05/02/37/sunset-1373171_1280.jpg"
-      },
-      _id:"1",
+export const sampleNotifications = [
+  {
+    sender: {
+      name: "hulala",
+      avatar: "https://cdn.pixabay.com/photo/2016/05/05/02/37/sunset-1373171_1280.jpg"
     },
-    {
-      sender:{
-        name:"sdsdsd",
-        avatar:"https://cdn.pixabay.com/photo/2016/05/05/02/37/sunset-1373171_1280.jpg"
-      },
-      _id:"2",
-    }
-  ] 
+    _id: "1",
+  },
+  {
+    sender: {
+      name: "sdsdsd",
+      avatar: "https://cdn.pixabay.com/photo/2016/05/05/02/37/sunset-1373171_1280.jpg"
+    },
+    _id: "2",
+  }
+]
+
+
+export const sampleMsg = [
+  {
+    attachments: [
+      {
+        public_id: "asdsa",
+        url: "https://cdn.pixabay.com/photo/2017/09/01/00/15/png-2702691_640.png"
+      }
+    ],
+    content: "msg hia",
+    _id: "dsfsdafsdf",
+    sender: {
+      _id: "dsfsdf",
+      name: "ramu kaka"
+    },
+    chat: "chatId",
+    createdAt: "2024-04-28T19:44:52.692Z",
+  },
+  {
+    attachments: [
+      {
+        public_id: "asdsa",
+        url: "https://i.pinimg.com/originals/db/57/fb/db57fb79694494be4cb3906545c9bf7a.png"
+      }
+    ],
+    content: "kaisa h",
+    _id: "dsfsdfsdf",
+    sender: {
+      _id: "sdfsdf",
+      name: "dfsdf"
+    },
+    chat: "chatId",
+    createdAt: "2024-04-28T19:44:52.692Z",
+  },
+  {
+    attachments: [
+      {
+        public_id: "asdsa",
+        url: "https://cdn.pixabay.com/photo/2017/09/01/00/15/png-2702691_640.png"
+      }
+    ],
+    content: "msg hia",
+    _id: "dsfsdafsdfs",
+    sender: {
+      _id: "dsfsdf",
+      name: "ramu kaka"
+    },
+    chat: "chatId",
+    createdAt: "2024-04-28T19:44:52.692Z",
+  },
+  {
+    attachments: [
+      {
+        public_id: "asdsa",
+        url: "https://i.pinimg.com/originals/db/57/fb/db57fb79694494be4cb3906545c9bf7a.png"
+      }
+    ],
+    content: "kaisa h",
+    _id: "dsfsdfssdf",
+    sender: {
+      _id: "sdfsdf",
+      name: "dfsdf"
+    },
+    chat: "chatId",
+    createdAt: "2024-04-28T19:44:52.692Z",
+  }
+  
+]
