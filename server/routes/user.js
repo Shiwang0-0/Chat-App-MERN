3a26ea4b928152566f422bf7ace9f31f8159b744
Shiwang0-0 <bisht0shiwang@gmail.com>
2024-05-11 01:50:47 +0530

    my profile and logout routes

--- server/routes/user.js
+++ server/routes/user.js
@@ -1,11 +1,18 @@
 import express from "express";
-import { login, newUser } from "../controllers/user.js";
+import { login, newUser, myProfile, logout } from "../controllers/user.js";
 import { multerSingleAvatar} from "../middlewares/multer.js";
+import { isAuthenticated } from "../middlewares/auth.js";
 
 const app=express.Router();
 
 app.post("/new",multerSingleAvatar,newUser);
 
-app.get("/login",login);
+app.post("/login",login);
+
+app.use(isAuthenticated);
+
+app.get("/me",myProfile);
+
+app.get("/logout",logout);
 
 export default app;
