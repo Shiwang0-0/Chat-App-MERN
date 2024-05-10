9305a112c7a197a9df79061d9a6899df90f9a789
Shiwang0-0 <bisht0shiwang@gmail.com>
2024-05-11 01:11:27 +0530

    error middleware added

--- server/app.js
+++ server/app.js
@@ -2,6 +2,7 @@ import express from "express";
 import userRoute from "./routes/user.js"
 import { connectDB } from "./utils/database.js";
 import dotenv from "dotenv";
+import { errorMiddleware } from "./middlewares/error.js";
 
 const app=express();
 
@@ -14,11 +15,15 @@ const port=process.env.PORT || 3000;
 
 connectDB(uri);
 
+app.use(express.json());
+
 app.use("/user",userRoute);
 
 app.get("/",(req,res)=>{
     res.send("Welcome to Home")
 })
+
+app.use(errorMiddleware)
 
 app.listen(port,()=>
     {
