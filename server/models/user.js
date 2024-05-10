f8942a12b7c2b322625e0d74afae9ffe5f9355f7
Shiwang0-0 <bisht0shiwang@gmail.com>
2024-05-11 01:10:28 +0530

    added pre save functionality for any change in password

--- server/models/user.js
+++ server/models/user.js
@@ -1,4 +1,5 @@
 import mongoose,{ Schema,model, Types} from "mongoose";
+import {hash} from "bcrypt"
 
 const schema =new Schema({
     name:{
@@ -29,6 +30,14 @@ const schema =new Schema({
 {
     timestamps:true
 });
+
+schema.pre("save",async function(){
+    
+    if(!this.isModified("password")) 
+        return next();
+
+    this.password=await hash(this.password,10);
+})
 
 export const User=mongoose.models.User || model("User",schema)
 
