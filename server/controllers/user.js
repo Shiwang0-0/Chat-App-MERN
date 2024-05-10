1edf6ee6627e856db65d8f4dd5a531dd89c636e7
Shiwang0-0 <bisht0shiwang@gmail.com>
2024-05-11 01:11:15 +0530

    register and login functionality

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -1,24 +1,39 @@
+import { compare } from "bcrypt";
 import { User } from "../models/user.js"
+import { sendtoken } from "../utils/token.js";
+import { tryCatch } from "../middlewares/error.js";
+import { customError } from "../middlewares/error.js";
 
-const newUser=async(req,res)=>{
+const newUser=tryCatch(async(req,res)=>{
+
+    const {name,username,password}=req.body;
 
     const avatar={
         public_id:"sdfsdf",
         url:"Sfsdf"
     }
 
-    const user= await User.create({
-        name:"chonky",
-        username:"chonky_cat",
-        password:"cat",
-        avatar
-    })
+    const user= await User.create({name,username,password,avatar});
+    if(!user)
+        return next(new customError("Error, Please try again",500));
 
-    res.status(201).json({message:"user created successfully"})
-}
+    sendtoken(res,user,201,"User created")
+})
 
-const login=(req,res)=>{
-    res.send("helo")
-}
+const login = tryCatch(async(req,res,next)=>{
+    
+        const {username,password}=req.body;
+        const user=await User.findOne({username}).select("+password");
+    
+        if(!user)
+            return next(new customError("Invalid Credentials",401));
+    
+        const isPassword=await compare(password,user.password);
+    
+        if(!isPassword)
+            return next(new customError("Invalid Credentials",401));
+    
+        sendtoken(res,user,201,"User Logged in")   
+})
 
 export {login,newUser};
