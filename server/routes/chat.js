5fdb9c9a65efbcafaad02350abb51ce75a29e1f7
Shiwang0-0 <bisht0shiwang@gmail.com>
2024-05-20 04:57:01 +0530

    routes for all api calls with validations

--- server/routes/chat.js
+++ server/routes/chat.js
@@ -1,19 +1,26 @@
 import express from "express";
-import { addMembers, createGroup, getMyChats, getMyGroups, removeMembers, leaveGroup } from "../controllers/chat.js";
+import { addMembers, chatMessages, createGroup, deleteChats, getChatDetail, getMyChats, getMyGroups, leaveGroup, removeMembers, renameGroup, sendAttachment } from "../controllers/chat.js";
+import { addMemberValidator, chatIdInParamValidator, createGroupValidator, removeMemberValidator, renameGroupValidator, sendAttachmentValidator, validationHandler, } from "../lib/validators.js";
 import { isAuthenticated } from "../middlewares/auth.js";
+import { multerAttachments } from "../middlewares/multer.js";
 
 const app=express.Router();
 
 app.use(isAuthenticated);
 
-app.post("/newgroup",createGroup);
+app.post("/newgroup", createGroupValidator(), validationHandler, createGroup);
 
 app.get("/mychat",getMyChats);
 app.get("/mygroups",getMyGroups);
 
-app.put("/addmembers",addMembers);
-app.put("/removemembers",removeMembers);
+app.put("/addmembers", addMemberValidator(), validationHandler, addMembers);
+app.put("/removemembers", removeMemberValidator(), validationHandler, removeMembers);
 
-app.delete("/leave/:id",leaveGroup);
+app.delete("/leave/:id", chatIdInParamValidator(), validationHandler, leaveGroup);
+
+app.post("/message",multerAttachments, sendAttachmentValidator(), validationHandler, sendAttachment);
+app.get("/message/:id", chatIdInParamValidator(), validationHandler, chatMessages);
+
+app.route("/:id").get(chatIdInParamValidator(), validationHandler, getChatDetail).put(renameGroupValidator(), validationHandler, renameGroup).delete(chatIdInParamValidator(), validationHandler, deleteChats)
 
 export default app;
