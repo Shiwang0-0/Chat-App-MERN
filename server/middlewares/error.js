c9b2f067775a458f1917811f3a5cd9ac10469266
Shiwang0-0 <bisht0shiwang@gmail.com>
2024-05-20 04:55:35 +0530

    added handler for duplicate key and cast error

--- server/middlewares/error.js
+++ server/middlewares/error.js
@@ -1,6 +1,20 @@
 const errorMiddleware=(err,req,res,next)=>{
     err.message=err.message || "Sorry, Please try again";
     err.statusCode=err.statusCode || 500;
+    
+    if(err.code === 11000)
+        {
+            const errorField=Object.keys(err.keyPattern).join(",")
+            err.message=`Duplicate field ${errorField}`
+            err.statusCode=409;
+        }
+
+    if(err.name==="CastError")
+        {
+            const path=err.path
+            err.message=`Invalid format of ${path}`
+            err.statusCode=409;
+        }
 
     return res.status(err.statusCode).json({
         success:false,
@@ -11,7 +25,7 @@ const errorMiddleware=(err,req,res,next)=>{
 
 const tryCatch=(func)=> async(req,res,next)=>{
     try{
-        func(req,res,next);
+        await func(req,res,next);
     }
     catch(error)
     {
