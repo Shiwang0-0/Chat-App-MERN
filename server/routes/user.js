0b6878a0e702149e08285d8171d2d7f119915350
Shiwang0-0 <bisht0shiwang@gmail.com>
2024-05-20 04:57:28 +0530

    routes for all user api with validations in between

--- server/routes/user.js
+++ server/routes/user.js
@@ -1,18 +1,27 @@
 import express from "express";
-import { login, newUser, myProfile, logout } from "../controllers/user.js";
-import { multerSingleAvatar} from "../middlewares/multer.js";
+import { acceptFriendRequest, availableFriends, login, logout, myProfile, newUser, notifications, searchUser, sendFriendRequest } from "../controllers/user.js";
+import { loginValidator, registerValidator, requestAcceptorValidator, requestSenderValidator, validationHandler } from "../lib/validators.js";
 import { isAuthenticated } from "../middlewares/auth.js";
+import { multerSingleAvatar } from "../middlewares/multer.js";
 
 const app=express.Router();
 
-app.post("/new",multerSingleAvatar,newUser);
+app.post("/new",multerSingleAvatar, registerValidator(), validationHandler, newUser);
 
-app.post("/login",login);
+app.post("/login", loginValidator(), validationHandler, login);
 
 app.use(isAuthenticated);
 
 app.get("/me",myProfile);
 
 app.get("/logout",logout);
+
+app.get("/search",searchUser);
+
+app.post("/request",requestSenderValidator(), validationHandler, sendFriendRequest);
+app.post("/handlerequest",requestAcceptorValidator(), validationHandler, acceptFriendRequest);
+app.get("/notifications",notifications);
+
+app.get("/friends",availableFriends);
 
 export default app;
