925b9228953da374869124cfce6a2fceeb3344a5
Shiwang0-0 <bisht0shiwang@gmail.com>
2024-05-22 07:51:51 +0530

    fetching user mapped socket id

--- server/lib/helper.js
+++ server/lib/helper.js
@@ -1,3 +1,11 @@
+import { usersSocketIds } from "../app.js";
+
 export const getOtherMember=(members,userId)=>{
     return members.find((members)=>members._id.toString()!==userId.toString());
+}
+
+export const usersSockets=(members=[])=>{
+    const sockets=members.map((i)=>usersSocketIds.get(i._id.toString()))
+
+    return sockets;
 }
