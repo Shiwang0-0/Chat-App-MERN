b91c28d75cf26e161db3e715662ccb4da880e9ee
Shiwang0-0 <bisht0shiwang@gmail.com>
2024-06-16 20:31:43 +0530

    Login and Register form frontend-backend integrated

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,62 +1,121 @@
-import React, { useEffect, useState } from 'react'
-import {Container,Paper, Typography, TextField,Button,Box,IconButton,Stack,Avatar} from '@mui/material';
 import CameraAltIcon from '@mui/icons-material/CameraAlt';
-import { VisuallyHiddenInput } from '../components/styles/StyledComponent';
-import { validate} from '../utils/validation';
-
+import { Avatar, Box, Button, Container, IconButton, Paper, Stack, TextField, Typography } from '@mui/material';
+import axios from 'axios';
+import React, { useState } from 'react';
+import toast from 'react-hot-toast';
+import { useDispatch } from 'react-redux';
+import { server } from '../constants/configServer';
+import { userExist } from '../redux/reducers/auth';
+import { validate } from '../utils/validation';
 
 const Login = () => {
 
-  const [isLogin, setIsLogin]=useState(true);
-  const togglePage=()=>{
-    setIsLogin(((prev)=>!prev));
-  }
+  const dispatch=useDispatch();
+
   const initialValuesLogin = { username: "",password: "" };
-  const initialValuesRegister = { username: "",password: "" ,confirmpassword:""};
+  const initialValuesRegister = { name:"",username: "",password: "" ,confirmpassword:""};
+  const allowedExtensions = ['jpeg', 'jpg', 'png'];
+  const [isLogin, setIsLogin]=useState(true);
   const [formValues, setFormValues] = useState(isLogin?initialValuesLogin:initialValuesRegister);
   const [formErrors, setFormErrors] = useState({});
-  const [isSubmit, setIsSubmit] = useState(false);
   const [selectedImg, setSelectedImg] = useState(null);
   const [fileError,setFileError]=useState(null);
-  const allowedExtensions=['jpeg' || 'jpg' || 'png'];
+  
+  const togglePage=()=>{
+    setIsLogin(((prev)=>!prev));
+    setFormValues(isLogin?initialValuesRegister:initialValuesLogin);
+    setFormErrors({});
+    setSelectedImg(null);
+  }
 
   const handleChange = (e) => {
     const { name, value } = e.target;
     setFormValues({ ...formValues, [name]: value });
+    setFormErrors(prevErrors => ({ ...prevErrors, [name]: '' })); 
   };
-
-  const handleSubmit = (e) => { 
-    e.preventDefault();
-    const errors = validate(formValues,isLogin);
-    setFormErrors(errors);
-    setIsSubmit(true);
-  };
-
-  useEffect(() => {
-    console.log(formErrors);
-    if (Object.keys(formErrors).length === 0 && isSubmit) {
-      console.log(formValues);
+  
+  const handleImgChange = (e) => {
+    const file = e.target.files[0];
+    if (!file) {
+      return;
     }
-  }, [formErrors,isSubmit]);
-
-  const handleImgChange=(e)=>{
-    const file=e.target.files[0];
-    const fileName=file.name;
-    const fileExtension=fileName.split('.').pop();
-    const ifEndsWith= allowedExtensions.some(ext => fileExtension);
-    if(fileName && ifEndsWith)
-    {
-      const reader=new FileReader();
-      reader.onload=(e)=>{
+  
+    const fileName = file.name;
+    const fileExtension = fileName.split('.').pop().toLowerCase();
+  
+    if (allowedExtensions.includes(fileExtension)) {
+      const reader = new FileReader();
+      reader.onload = (e) => {
         setSelectedImg(e.target.result);
-      }
+      };
       reader.readAsDataURL(file);
+      setFileError(null);
+      setFormValues({ ...formValues, avatar: file });
+    } 
+    else {
+      setFileError('Please upload a file with a .jpg, .png, or .jpeg extension');
+      setSelectedImg(null);
     }
-    else
-    {
-      setFileError('Please upload a file with a .jpg or .png or .jpeg extension');
+  };
+
+
+  const handleSubmit = isLogin ? async (e) => {
+    e.preventDefault();
+    const errors = validate(formValues, isLogin);
+    setFormErrors(errors);
+    if (Object.keys(errors).length !== 0) {
+      return;
     }
-  }
+    
+    try {
+      const data = await axios.post(`${server}/api/v1/user/login`,
+        {
+          username: formValues.username,
+          password: formValues.password,
+        },
+        {
+          withCredentials: true,
+          headers: {
+            "Content-Type": "application/json"
+          }
+        }
+      );
+      dispatch(userExist(true));
+      toast.success(data.message);
+    } catch (error) {
+      toast.error(error?.response?.data?.message || "Something went wrong");
+    }
+  } 
+  : async (e) => {
+    e.preventDefault();
+    const errors = validate(formValues, isLogin);
+    setFormErrors(errors);
+    if (Object.keys(errors).length !== 0) {
+      return;
+    }
+    try {
+      const formData = new FormData();
+      formData.append("avatar", formValues.avatar);
+      formData.append("name", formValues.name);
+      formData.append("username", formValues.username);
+      formData.append("password", formValues.password);
+      formData.append("confirmpassword", formValues.confirmpassword);
+  
+    
+      const data = await axios.post(`${server}/api/v1/user/register`, formData, {
+        withCredentials: true,
+        headers: {
+          "Content-Type": "multipart/form-data",
+        },
+      });
+      dispatch(userExist(true));
+      toast.success(data.message);
+    } catch (error) {
+      toast.error(error?.response?.data?.message || "Something went wrong");
+    }
+  };
+  
+
 
   return (
     <>
@@ -114,20 +173,30 @@ const Login = () => {
         </Typography>
 
         <Stack position={"relative"}>
-          <Avatar src={selectedImg}
-          sx={{width:"10rem",height:"10rem",mt:"13px",objectFit:"contained"}} 
-          />
-          <input type="file" onChange={handleImgChange} style={{ display: 'none' }} accept="image/*" id="avatar-input"/>
-          {fileError && <p style={{margin:"-5px",color:"red"}}>{fileError}</p>}
-          <label htmlFor='avatar-input'>
-          <IconButton sx={{position:"absolute" ,bottom: "0" ,right: "0"}} component="span">
-            <CameraAltIcon/>  
-            <VisuallyHiddenInput type="image/*"/>  
+        <Avatar src={selectedImg}
+          sx={{ width: "10rem", height: "10rem", mt: "7px", objectFit: "contained" }}
+        />
+        <input type="file" name="avatar" onChange={handleImgChange} style={{ display: 'none' }} accept="image/*"      id="avatar-input" />
+        {fileError && <p style={{ margin: "-5px", color: "red" }}>{fileError}</p>}
+        <label htmlFor='avatar-input'>
+          <IconButton sx={{ position: "absolute", bottom: "0", right: "0" }} component="span">
+            <CameraAltIcon />
           </IconButton>
-          </label>
-        </Stack>
+        </label>
+      </Stack>
 
-        <form style={{display:"flex",flexDirection:"column",alignItems:"center", width:"80%", margin:"10px"}} onSubmit={handleSubmit}>
+        <form style={{display:"flex",flexDirection:"column",alignItems:"center", width:"80%", margin:"8px"}} onSubmit={handleSubmit}>
+        <TextField 
+            margin='normal'
+            required
+            fullWidth
+            name="name"
+            label="Name"  
+            variant='outlined'
+            value={formValues.name || ''}
+            onChange={handleChange}
+          />
+          {formErrors.name && <p style={{margin:"-5px",color:"red"}}>{formErrors.name}</p>}
         <TextField 
             margin='normal'
             required
@@ -165,9 +234,9 @@ const Login = () => {
             Sign Up
             </Button>
           </Box>
-          <Typography sx={{mt:"38px",display:"flex",justifyContent:"center",alignItems:"center", textAlign:"center"}}>Already a User?</Typography>
+          <Typography sx={{mt:"25px",display:"flex",justifyContent:"center",alignItems:"center", textAlign:"center"}}>Already a User?</Typography>
           
-          <Box sx={{display:"flex",borderRadius:"1rem",alignItems:"center" ,justifyContent:"center", mt:"8px"}}>
+          <Box sx={{display:"flex",borderRadius:"1rem",alignItems:"center" ,justifyContent:"center", mt:"4px"}}>
             <Button type="button" onClick={togglePage} >
             Login
             </Button>
@@ -177,8 +246,6 @@ const Login = () => {
         </Paper>
       </>
       }
-     
-    
     </Container>
     </>
   )
--- client/src/utils/validation.js
+++ client/src/utils/validation.js
@@ -1,20 +1,27 @@
-export const validate= (values,isLogin) => {
-    const errors = {};
-    if (!values.username) {
-      errors.username = "Username is required!";
-    }
-    else if (values.username.length < 2) {
-      errors.username = "username must be more than 2 characters";
-    }
-    if (!values.password) {
-      errors.password = "Password is required";
-    } else if (values.password.length < 6) {
-      errors.password = "Password must be more than 6 characters";
-    } else if (values.password.length > 10) {
-      errors.password = "Password cannot exceed more than 10 characters";
-    }
-    if(!isLogin && values.confirmpassword !== values.password){
+export const validate = (values, isLogin) => {
+  const errors = {};
+  if (!values.username) {
+    errors.username = "Username is required!";
+  } else if (values.username.length < 2) {
+    errors.username = "Username must be more than 2 characters";
+  }
+  if (!values.password) {
+    errors.password = "Password is required";
+  } else if (values.password.length < 6) {
+    errors.password = "Password must be more than 6 characters";
+  } else if (values.password.length > 10) {
+    errors.password = "Password cannot exceed more than 10 characters";
+  }
+  if(!isLogin)
+  {
+      if (!values.name) {
+        errors.name= "Name is required";
+      } else if (values.name.length < 2) {
+        errors.name = "Name must be more than 2 characters";
+      }
+      if (values.confirmpassword !== values.password) {
         errors.confirmpassword = "Please type the same password as entered above";
-    }
-    return errors;
-  };
+      }
+  }
+  return errors;
+};
