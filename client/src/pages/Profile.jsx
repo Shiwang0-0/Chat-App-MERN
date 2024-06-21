bcdd5094ac09ddd7d0a831b5ee3b460d3d2384f8
Shiwang0-0 <bisht0shiwang@gmail.com>
2024-06-21 08:44:00 +0530

    fetched name,bio,email,creation date

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,21 +1,23 @@
-import React from 'react'
-import AppLayout from '../components/layout/AppLayout'
-import { Stack,Avatar ,Box, Typography} from '@mui/material'
-import { random } from '../constants/colors'
 import AssignmentIndIcon from '@mui/icons-material/AssignmentInd';
+import CalendarMonthIcon from '@mui/icons-material/CalendarMonth';
 import EmailIcon from '@mui/icons-material/Email';
 import TextSnippetIcon from '@mui/icons-material/TextSnippet';
-import CalendarMonthIcon from '@mui/icons-material/CalendarMonth';
-import moment from 'moment'
-const Profile = () => {
+import { Avatar, Box, Stack, Typography } from '@mui/material';
+import moment from 'moment';
+import React from 'react';
+import AppLayout from '../components/layout/AppLayout';
+import { random } from '../constants/colors';
+import { transformImage } from '../lib/fileFormat';
+const Profile = ({user}) => {
+  
   return (
     <>
     <Box sx={{ display:"flex",flexDirection:"column",alignItems:"center",mt:"50px", height:"450px", widht:"100%",backgroundColor:random}}>
-        <Avatar sx={{height:"150px", width:"150px", objectFit:"contain",mt:"10px"}} />
-        <ProfileCard heading="bio" text="iam a kitty" icon={<TextSnippetIcon/>} />
-        <ProfileCard heading="username" text="ChonkyCat" icon={<AssignmentIndIcon/>} />
-        <ProfileCard heading="email" text="feedMe@kitty" icon={<EmailIcon/>} />
-        <ProfileCard heading="joined" text={moment('2024-04-18T00:00:00.000Z').fromNow()} icon={<CalendarMonthIcon/>} />
+        <Avatar src={transformImage(user?.avatar?.url)} sx={{height:"150px", width:"150px", objectFit:"contain",mt:"10px"}} />
+        <ProfileCard heading="bio" text={user?.bio || ""} icon={<TextSnippetIcon/>} />
+        <ProfileCard heading="username" text={user?.username} icon={<AssignmentIndIcon/>} />
+        <ProfileCard heading="email" text={user?.email} icon={<EmailIcon/>} />
+        <ProfileCard heading="joined" text={moment(user?.createdAt).fromNow()} icon={<CalendarMonthIcon/>} />
     </Box> 
     </> 
   )
