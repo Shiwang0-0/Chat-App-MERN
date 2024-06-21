2da2e6971bd2c058ad982ca49dc4b87a82239838
Shiwang0-0 <bisht0shiwang@gmail.com>
2024-06-21 08:48:26 +0530

    fixed name from avatar to groupAvatar

--- client/src/components/specific/ChatList.jsx
+++ client/src/components/specific/ChatList.jsx
@@ -1,4 +1,4 @@
-import { Stack, Typography,Box } from '@mui/material'
+import { Box, Stack } from '@mui/material'
 import React from 'react'
 import { royalBlue } from '../../constants/colors'
 import ChatItem from '../shared/ChatItem'
@@ -23,13 +23,13 @@ const ChatList = (
           
           chats?.map((data,index)=>{
 
-            const {avatar,_id,name,groupChat,members}=data;
+            const {groupAvatar,_id,name,groupChat,members}=data;
             const newMessageAlert=newMessagesAlert.find(
                 ({chatId})=>chatId===_id
             )
             const isOnline=members?.some((member)=>onlineUser.includes(_id));
             return (
-            <ChatItem index={index} newMessageAlert={newMessageAlert} isOnline={isOnline} avatar={avatar} name={name} _id={_id} key={_id} groupChat={groupChat} sameSender={chatId===_id} handleDeleteChat={handleDeleteChat}/>
+            <ChatItem index={index} newMessageAlert={newMessageAlert} isOnline={isOnline} avatar={groupAvatar} name={name} _id={_id} key={_id} groupChat={groupChat} sameSender={chatId===_id} handleDeleteChat={handleDeleteChat}/>
           ) 
           }
           )}
