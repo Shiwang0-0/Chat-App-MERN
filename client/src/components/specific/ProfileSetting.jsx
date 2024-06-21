b58eedf1b8920ea1f65059835a6ba4dd10362e8f
Shiwang0-0 <bisht0shiwang@gmail.com>
2024-06-21 08:46:58 +0530

    added logout functionlity

--- client/src/components/specific/ProfileSetting.jsx
+++ client/src/components/specific/ProfileSetting.jsx
@@ -1,16 +1,36 @@
-import React from 'react'
-import { Box } from '@mui/material'
-import { royalBlue } from '../../constants/colors'
-import { NavigationBars } from '../shared/NavigationBars';
-import SupervisedUserCircleIcon from '@mui/icons-material/SupervisedUserCircle';
 import AutoDeleteIcon from '@mui/icons-material/AutoDelete';
 import BlockIcon from '@mui/icons-material/Block';
 import EditIcon from '@mui/icons-material/Edit';
+import LogoutIcon from '@mui/icons-material/Logout';
+import SupervisedUserCircleIcon from '@mui/icons-material/SupervisedUserCircle';
+import { Box } from '@mui/material';
+import axios from 'axios';
+import React from 'react';
+import toast from 'react-hot-toast';
+import { useDispatch } from 'react-redux';
+import { royalBlue } from '../../constants/colors';
+import { server } from '../../constants/configServer';
+import { userNotExist } from '../../redux/reducers/auth';
+import { NavigationBars } from '../shared/NavigationBars';
 const ProfileSetting = () => {
 
-    const navigateToHome=()=>{
-        console.log("home");
+  const dispatch=useDispatch();
+
+  const navigateToHome=()=>{
         navigate("/");
+    }
+
+    const logout=async()=>{
+      try{
+        const {data}=await axios.get(`${server}/api/v1/user/logout`,{
+          withCredentials:true,
+        });
+        dispatch(userNotExist())
+        toast.success(data.message)
+      }
+      catch(error){
+        toast.error(error?.response?.data?.message || "Something went wrong")
+      }
     }
 
   return (
@@ -20,6 +40,7 @@ const ProfileSetting = () => {
       <NavigationBars title={"Add an Exisiting Account"} icon={<SupervisedUserCircleIcon/>} onClickfunc={navigateToHome}/>
       <NavigationBars title={"Deactivate account"} icon={<AutoDeleteIcon/>} onClickfunc={navigateToHome}/>
       <NavigationBars title={"Blocked users"} icon={<BlockIcon/>} onClickfunc={navigateToHome}/>
+      <NavigationBars title={"Logout"} icon={<LogoutIcon/>} onClickfunc={logout}/>
       </Box>
     </div>
   )
