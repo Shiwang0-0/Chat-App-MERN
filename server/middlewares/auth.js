11dccccce99b0ecf304de59a6f8710ce8abe35a7
Shiwang0-0 <bisht0shiwang@gmail.com>
2024-06-27 17:24:20 +0530

    socket authenticator added

--- server/middlewares/auth.js
+++ server/middlewares/auth.js
@@ -1,9 +1,11 @@
-import { customError } from "./error.js";
+import { TOKEN } from "../constants/config.js";
+import { User } from "../models/user.js";
+import { customError, tryCatch } from "./error.js";
 import jwt from "jsonwebtoken";
 
-const isAuthenticated =(req,res,next)=>{
+const isAuthenticated =tryCatch((req,res,next)=>{
 
-    const token=req.cookies["val-token"];
+    const token=req.cookies[TOKEN];
     
     if(!token)
         return next(new customError("Login required",401))
@@ -15,6 +17,34 @@ const isAuthenticated =(req,res,next)=>{
 
     req.user=decodedToken;
     next();
+})
+
+
+const socketAuthenticator=async(err,socket,next)=>{
+
+    try{
+        if(err)
+            return next(customError("Please Login to access this route",401));
+    
+        const authToken=socket.request.cookies[TOKEN];
+    
+        if(!authToken)
+            return next(customError("Please Login to access this route",401));
+    
+        const decodedData=jwt.verify(authToken,process.env.JWT_SECRET)
+    
+        const user=await User.findById(decodedData._id);
+    
+        if(!user)
+            return next(customError("User does not exist",401));
+    
+        socket.user=user;
+        return next();
+    }
+    catch(error)
+    {
+        return next(error);
+    }
 }
 
-export {isAuthenticated};
+export {isAuthenticated,socketAuthenticator};
